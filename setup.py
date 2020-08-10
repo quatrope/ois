158873bf2e90c381e758e540d1ef05ba2155e606
Martin Beroiz <martinberoiz@gmail.com>
2020-08-10 15:24:09 -0500

    fix code coverage report

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ varconv = Extension('varconv',
                     libraries = ['m'],
                     extra_compile_args=["-std=c99"])
 
-
 setup(name='ois',
       version=ois_version,
       description='Optimal Image Subtraction',
@@ -28,7 +27,6 @@ setup(name='ois',
       url='https://github.com/toros-astro/ois',
       py_modules=['ois', ],
       ext_modules=[varconv],
-      # include_dirs=[numpy.get_include()],
       install_requires=["numpy>=1.6",
                         "scipy>=0.16"],
       test_suite='tests',
